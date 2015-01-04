639721658d19787a57a769f0fe1bd7e1f14453a8
Marcus Correa <marcuscorrea@live.com>
2015-01-04 15:07:43 -0200

    Adicionado PHPInfo

--- index.php
+++ index.php
@@ -15,5 +15,9 @@
     <!-- Javascript -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+
+    <?php
+        phpinfo();
+    ?>
 </body>
 </html>
